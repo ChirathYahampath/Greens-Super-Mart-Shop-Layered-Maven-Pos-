de7540866299a807e9cf5072e081097f1047a903
Chirath Yahampath <141777544+ChirathYahampath@users.noreply.github.com>
2023-12-22 19:37:27 +0530

    Layered

---
+++ src/main/java/Controller/DashBoardFormController.java
@@ -0,0 +1,46 @@
+package Controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import javax.xml.namespace.QName;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DashBoardFormController {
+    public JFXButton CustomerBtn;
+    public AnchorPane Pane;
+
+    public void CustomerButtonOnAction(ActionEvent actionEvent) {
+        Stage stage= (Stage) Pane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/CustomerForm.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void itemButtonOnAction(ActionEvent actionEvent) {
+        Stage stage= (Stage) Pane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/itemform.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
