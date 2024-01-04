a85507d924e909375f6678d44eab034207465322
Chirath Yahampath <141777544+ChirathYahampath@users.noreply.github.com>
2024-01-04 14:37:31 +0530

    implement OrderDeatilBo & OrderDeatilBoimpl

--- src/main/java/bo/custom/impl/OrderBoImpl.java
+++ src/main/java/bo/custom/impl/OrderBoImpl.java
@@ -1,39 +1,21 @@
 package bo.custom.impl;
 
+import bo.BoFactory;
 import bo.custom.OrderBo;
+import dao.DaoFactory;
 import dao.custom.OrderDao;
-import dao.custom.impl.OrderDaoImpl;
+import dao.util.DaoType;
 import dto.OrderDto;
 
-import java.sql.SQLException;
-
 public class OrderBoImpl implements OrderBo {
-    private OrderDao orderDao = new OrderDaoImpl();
-
+    private OrderDao orderDao = DaoFactory.getInstance().getDao(DaoType.ORDER);
     @Override
-    public boolean saveOrder(OrderDto dto) throws SQLException, ClassNotFoundException {
-        return orderDao.save(dto);
+    public boolean saveOrder(OrderDto dto) {
+        return false;
     }
 
     @Override
-    public String generateId() throws SQLException, ClassNotFoundException {
-        try {
-            OrderDto dto = orderDao.getLastOrder();
-            if (dto!=null){
-                String id = dto.getOrderId();
-                int num = Integer.parseInt(id.split("[D]")[1]);
-                num++;
-                return String.format("D%03d",num);
-            }else{
-                return "D001";
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
+    public String generateId() {
+        return null;
     }
-
-
 }
